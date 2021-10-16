0664fefefb4aa410b8a6470f8add96143c5f481e
RicCada <riccardo.cadamuro@skywarder.eu>
2021-10-16 22:01:01 +0200

    Update comments

--- orbit2D.m
+++ orbit2D.m
@@ -1,43 +1,52 @@
 function [] = orbit2D(pfPar, nFig, firstTime)
-    
-    a = pfPar(1); 
+%orbit2D(pfPar, nFig, firstTime): permette di stampare in 2D l'orbita di un corpo dati
+%i parametri nel SdR Perifocale.
+%
+%   INPUT:
+%       pfPar: parametri nel SdR Perifocale: nell'ordine: [a, e, omega, theta]
+%       nFig: ID della figura su cui andare a disegnare l'orbita
+%       firstTime: booleano per sapere se è la prima orbita che si disegna
+%       sulla data figura, in modo da poter stampare più orbite coplanari
+%       nella stessa immagine
+%   L'asse x della figura è allineato con la linea dei nodi, che risulta
+%   costante per orbite coplanari.    a = pfPar(1); 
     e = pfPar(2); 
     omega = pfPar(3); 
     theta = pfPar(4); 
     
-thetaV = linspace(0, 360, 200);                               % vettore degli angoli
-p = a*(1-e^2);                                                % semilato retto
-rV = p./(1 + e*cosd(thetaV));                                 % punti dell'ellisse
+    thetaV = linspace(0, 360, 200);                               % vettore degli angoli
+    p = a*(1-e^2);                                                % semilato retto
+    rV = p./(1 + e*cosd(thetaV));                                 % punti dell'ellisse
 
     xOrb = rV.*cosd(thetaV); 
     yOrb = rV.*sind(thetaV); 
     r2D = [xOrb ; yOrb];
 
-Rot2D = [cosd(omega) -sind(omega); sind(omega) cosd(omega)];  % matrice di rotazione per allineare l'asse x con la linea dei nodi
+    Rot2D = [cosd(omega) -sind(omega); sind(omega) cosd(omega)];  % matrice di rotazione per allineare l'asse x con la linea dei nodi
 
-r2D = Rot2D * r2D;                                            % rotazione ellisse
+    r2D = Rot2D * r2D;                                            % rotazione ellisse
 
-r0 = Rot2D* (p/(1+e*cosd(theta)))*[cosd(theta); sind(theta)]; % posizione satellite
-rP = Rot2D* (p/(1+e))*[1; 0];                                 % posizione pericentro
-rA = Rot2D* (p/(1-e))*[-1; 0];                                % posizione apocentro
+    r0 = Rot2D* (p/(1+e*cosd(theta)))*[cosd(theta); sind(theta)]; % posizione satellite
+    rP = Rot2D* (p/(1+e))*[1; 0];                                 % posizione pericentro
+    rA = Rot2D* (p/(1-e))*[-1; 0];                                % posizione apocentro
 
     figure(nFig); 
-plot(r2D(1, :),r2D(2, :));                                    % plot orbit
+    plot(r2D(1, :),r2D(2, :));                                    % plot orbit
     
-if firstTime                                                  % se è la prima volta che apre la figura disegna la terra
-rt=6371;                                                      % raggio della Terra [km]
-    rectangle('Position', [-rt,-rt, 2*rt, 2*rt], ...
-'Curvature', [1 1], 'FaceColor','b');                         % plot terra in scala
-    hold on
-    grid on;
-    title("2D ORBIT");
-    xlabel("Line of nodes N [km]");
-    ylabel("y [km]");
-end
+    if firstTime                                                  % se è la prima volta che apre la figura disegna la terra
+        rt=6371;                                                  % raggio della Terra [km]
+        rectangle('Position', [-rt,-rt, 2*rt, 2*rt], ...
+            'Curvature', [1 1], 'FaceColor','b');                 % plot terra in scala
+        hold on
+        grid on;
+        title("2D ORBIT");
+        xlabel("Line of nodes N [km]");
+        ylabel("y [km]");
+    end
     
-plot(r0(1), r0(2), 'hr');                                     % plot satellite
-plot(rA(1), rA(2), '.r', 'LineWidth', 4);                     % plot apoapsis
-plot(rP(1), rP(2), '.r', 'LineWidth', 4);                     % plot periapsis
+    plot(r0(1), r0(2), 'hr');                                     % plot satellite
+    plot(rA(1), rA(2), '.r', 'LineWidth', 4);                     % plot apoapsis
+    plot(rP(1), rP(2), '.r', 'LineWidth', 4);                     % plot periapsis
     axis equal;
     xlim([-36e3,+36e3])
     ylim([-36e3,+36e3])
--- orbit3D.m
+++ orbit3D.m
@@ -1,41 +1,50 @@
 function [] = orbit3D (pfPar, nFig)
+%orbit3D(pfPar, nFig): permette di stampare in 3D l'orbita di un corpo dati
+%i parametri nel SdR Perifocale.
+%
+%   INPUT:
+%       pfPar: parametri nel SdR Perifocale: nell'ordine: [a, e, i, RAAN,
+%       omega, theta]
+%       nFig: ID della figura su cui andare a disegnare l'orbita
+%   ATTENZIONE: prima di chiamare questa funzione è consigliato chiamare
+%       earth3D in modo da stampare anche la Terra come riferimanto
 
-a = pfPar(1);
-e = pfPar(2);
-i = pfPar(3);
-RAAN = pfPar(4);
-omega = pfPar(5);
-theta = pfPar(6);
+    a = pfPar(1);
+    e = pfPar(2);
+    i = pfPar(3);
+    RAAN = pfPar(4);
+    omega = pfPar(5);
+    theta = pfPar(6);
 
-thetaV = linspace(0, 360, 200);                             % vettore angoli da 0 a 360
-p = a*(1-e^2);                                               % semilato retto
-rV = p./(1 + e*cosd(thetaV));                              % punti dell'ellisse
+    thetaV = linspace(0, 360, 200);                             % vettore angoli da 0 a 360
+    p = a*(1-e^2);                                               % semilato retto
+    rV = p./(1 + e*cosd(thetaV));                              % punti dell'ellisse
 
-xOrb = rV.*cosd(thetaV);                                  % punti x dell'ellisse
-yOrb = rV.*sind(thetaV);                                  % punti y dell'ellisse
-r2D = [xOrb ; yOrb; zeros(1, 200)];                       % ellisse nel piano
+    xOrb = rV.*cosd(thetaV);                                  % punti x dell'ellisse
+    yOrb = rV.*sind(thetaV);                                  % punti y dell'ellisse
+    r2D = [xOrb ; yOrb; zeros(1, 200)];                       % ellisse nel piano
 
-R = RotPF2GE(i, RAAN, omega);                                % matrice di rotazione da perifocale a geocentrico
+    R = RotPF2GE(i, RAAN, omega);                                % matrice di rotazione da perifocale a geocentrico
 
-r3D = R*r2D;                                               % rotazione ellisse nello spazio
+    r3D = R*r2D;                                               % rotazione ellisse nello spazio
 
-r0 = R* (p/(1+e*cosd(theta)))*[cosd(theta); sind(theta); 0]; % posizione satellite
-rP = R* (p/(1+e))*[1; 0; 0];                                 % posizione pericentro
-rA = R* (p/(1-e))*[-1; 0; 0];                                % posizione apocentro
+    r0 = R* (p/(1+e*cosd(theta)))*[cosd(theta); sind(theta); 0]; % posizione satellite
+    rP = R* (p/(1+e))*[1; 0; 0];                                 % posizione pericentro
+    rA = R* (p/(1-e))*[-1; 0; 0];                                % posizione apocentro
 
 
-figure(nFig);
-hold on;
+    figure(nFig);
+    hold on;
 
-plot3(r3D(1,:),r3D(2,:),r3D(3, :));                       % plot orbita
-plot3(r0(1), r0(2), r0(3),  'hr');                           % plot satellite
-plot3(rA(1), rA(2), rA(3), '.r');                            % plot apocentro
-plot3(rP(1), rP(2), rP(3), '.r');                            % plot pericentro
-legend('Orbita','Satellite','Apocentro','Pericentro')
-view(45,15)                                                  % impostazioni visualizzazione
-axis equal
+    plot3(r3D(1,:),r3D(2,:),r3D(3, :));                       % plot orbita
+    plot3(r0(1), r0(2), r0(3),  'hr');                           % plot satellite
+    plot3(rA(1), rA(2), rA(3), '.r');                            % plot apocentro
+    plot3(rP(1), rP(2), rP(3), '.r');                            % plot pericentro
+    legend('Orbita','Satellite','Apocentro','Pericentro')
+    view(45,15)                                                  % impostazioni visualizzazione
+    axis equal
 
-grid on;
+    grid on;
 
 
 end
