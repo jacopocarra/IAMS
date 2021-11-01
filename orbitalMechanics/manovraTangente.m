fb4cf9cc8ddfd9b48f303c39ef149b2e730fc391
RicCada <riccardo.cadamuro@skywarder.eu>
2021-11-01 20:11:49 +0100

    Revert "manovraTangtente"
    This reverts commit 16c8164f5a47cb732c88398ff4a415faaa569cc3.

--- orbitalMechanics/manovraTangente.m
+++ orbitalMechanics/manovraTangente.m
@@ -1,66 +1,51 @@
-function [orbFin, deltaV , deltaT] = manovraTangente(orbIniz, aFin, type)
+function [deltaV, thetaMan, deltaT, deltaT1, deltaT2] = manovraTangente(orbIniz, orbFin, type)
 
-%   [orbFin, deltaV , deltaT] = manovraTangente(orbIniz, aFin, type)
-%   Calcola una manovra tangente
-%   
-%   Input: 
-%       orbIniz: orbita iniziale
-%       aFin: semiasse maggiore orbita obiettivo
-%       type:   'apo': manovro in corrispondenza dell'apocentro
-%               'per': manovro in corrispondenza del pericentro
-% 
-%   Output: 
-%       orbFin: orbita finale (theta in corrispondenza della fine della
-%               manovra)
-%       deltaV
-%       deltaT
+%{
+   'apo' --> manovra all'apocentro (theta = 180);
+   'peri' --> manovra al pericentro (theta = 0);
+   'general' --> manovra generica tangente nel punto di theta dell'orbita 1
 
-    type = lower(type); 
+   In 'apo' e 'peri' va automaticamente dal theta dell'orbita al theta di
+   manovra
+%}
+
     mu = 398600;
 
     aIniz = orbIniz(1);
     eIniz = orbIniz(2);
+    iIniz = orbIniz(3);
+    RAANIniz = orbIniz(4);
+    omegaIniz = orbIniz(5);
     thetaIniz = wrapTo360(orbIniz(6));
-    
-    orbFin = orbIniz; 
-    orbFin(1) = aFin; 
 
-    switch type
-        case 'apo'
-            
-            r1 = aIniz*(1+eIniz);  %raggio apocentro orbIniz
-          
-        case 'per'
-           
-            r1 = aIniz*(1-eIniz);  %raggio pericentro orbIniz
-    end
+    aFin = orbFin(1);
+    eFin = orbFin(2);
+    thetaFin = orbFin(6);
     
-    deltaV = abs( sqrt( 2*mu*((1/r1)-(1/(2*aFin))) ) - sqrt( 2*mu*((1/r1)-(1/(2*aIniz))) ) ); %calcolo il deltaV -> essendo in valore assoluto non importa il segno della sottrazione
-    
-    r2 = 2*aFin - r1; %calcolo il raggio al punto apsidale opposto
-    
-    if r2 > r1  %per capire che tipo di orbita risulterà
-        thetaMan = 0;  %manovro nel pericentro dell'orbita finale
-        
-        orbFin(2) = (r2 - r1)/(r1 + r2);
-        
-        if type == "apo"  %se avevo scelto di manovrare nell'apocentro dell'orbita iniziale devo cambiare l'anomalia al pericentro
-            orbFin(5) = wrapTo360(180 + orbFin(5));
-        end
-        
-    else
-        thetaMan = 180; %manovro nell'apocentro dell'orbita finale
-    
-        orbFin(2) = (r1 - r2)/(r1 + r2);
-        
-        if type == "per" %se avevo scelto di manovrare nel pericentro dell'orbita iniziale devo cambiare l'anomalia al pericentro
-            orbFin(5) = wrapTo360(180 + orbFin(5)); 
-        end
-    end 
-    
-    
-    orbFin(6) = thetaMan; 
-    
-    deltaT = tempoVolo(orbIniz, thetaIniz, thetaMan);
-
+switch lower(type)
+    case 'apo'
+        thetaMan = 180;
+        r = aIniz*(1+eIniz);
+        deltaT1 = tempoVolo(orbIniz, thetaIniz, thetaMan);
+        deltaV = abs(sqrt(2*mu*((1/r)-(1/(2*aFin))))-sqrt(2*mu*((1/r)-(1/(2*aIniz)))));
+    case 'peri'
+        thetaMan = 0;
+        r = aIniz*(1-eIniz);
+        deltaT1 = tempoVolo(orbIniz, thetaIniz, thetaMan);
+        deltaV = abs(sqrt(2*mu*((1/r)-(1/(2*aFin))))-sqrt(2*mu*((1/r)-(1/(2*aIniz)))));
+    case 'general'
+        thetaMan = thetaIniz;
+        r = aIniz*(1-eIniz^2)/(1+eIniz*cosd(thetaMan));
+        vr = sqrt(mu/p)*eIniz*sind(thetaMan);
+        vTheta = sqrt(mu/p)*(1+eIniz*cosd(thetaMan));
+        v1 = sqrt(2*mu*((1/r)-(1/(2*aIniz))));
+        gamma = atand(vr/vTheta);
+        v2 = sqrt(2*mu*((1/r)-(1/(2*aFin))));
+        deltaT1 = 0;
+        deltaV = sqrt(v1^2+v2^2-v1*v2*cosd(gamma));
+end
+   
+    deltaT2 = tempoVolo(orbFin, thetaMan, thetaFin);
+    deltaT = deltaT1+deltaT2;
+     
 end
--- simulation/tester.m
+++ simulation/tester.m
@@ -16,25 +16,22 @@ global myMovie;
 global myFig;
 myMovie = struct('cdata',[],'colormap',[]);
 fps = 100;
-%% dati
+%%
 
 
-mu = 398600; 
+mu = 398600
 ptoIniz = [-3441.6408 -7752.3491 -3456.8431 ...
-            4.9270 -0.5369 -4.0350];                                 % [x y z vx vy vz]
+4.9270 -0.5369 -4.0350];                                 % [x y z vx vy vz]
+orbFin2 = [12930.0, 0.2055, rad2deg(1.5510)...
+rad2deg(2.6830),  rad2deg(0.4098), rad2deg(1.6940)]';  % [a e i RAAN omega theta]                                % [x y z vx vy vz]
 rIniz = [ptoIniz(1) ptoIniz(2) ptoIniz(3)]';                % vettore posizione
 vIniz = [ptoIniz(4) ptoIniz(5) ptoIniz(6)]';                % vettore velocità
-orbIniz = GEtoPF(rIniz, vIniz, mu);  % da GE coordinate PF
 
-
-orbFin = [12930.0, 0.2055, rad2deg(1.5510), rad2deg(2.6830),  rad2deg(0.4098), rad2deg(1.6940)]';  % [a e i RAAN omega theta]        
-
-%%
-orbFin2 = orbFin; 
+[orbIniz] = GEtoPF(rIniz, vIniz, mu) % da GE coordinate PF
 
 [orbFin1, deltaV1, deltaT1] = cambioInclinazione(orbIniz, orbFin2(3), orbFin2(4))
-[orbFin3, deltaV, deltaT, thetaman] = cambioAnomaliaPericentro(orbFin1, orbFin2(5))
-[deltaV2, deltaV3, deltaV4, orbTrasf, deltaT2, deltaT3, deltaT4, thetaMan] = manovraBitangenteEllittica(orbFin3, orbFin2, 'pa')
+[orbFin, deltaV, deltaT, thetaman] = cambioAnomaliaPericentro(orbFin1, orbFin2(5))
+[deltaV2, deltaV3, deltaV4, orbTrasf, deltaT2, deltaT3, deltaT4, thetaMan] = manovraBitangenteEllittica(orbFin, orbFin2, 'pa')
 Title = 'STRATEGY 1';
 Maneuv_name=[{'initial point'};{'change of plane'};{'change of P arg'};...
     {'first bitangent maneuver'};{'second bitangent maneuver'};...
@@ -43,13 +40,24 @@ earth3D(1);                                              % plot terra
 orbit3D(orbIniz, 1);         % plot 3D orbita iniziale
 orbit3D(orbFin2, 1);                                      % plot 3D orbita finale
 
-plotOrbit([orbIniz, orbFin1,orbFin3, orbTrasf,orbFin2],[orbIniz(6), orbFin1(6), orbFin1(6), thetaman, orbFin3(6), thetaMan, thetaMan, orbTrasf(6), orbTrasf(6) orbFin2(6)],[deltaT1, deltaT, deltaT2, deltaT3, deltaT4],Title,Maneuv_name,'dyn',0,[0, deltaV1, deltaV, deltaV2, deltaV3, deltaV4])
+plotOrbit([orbIniz, orbFin1,orbFin, orbTrasf,orbFin2],[orbIniz(6), orbFin1(6), orbFin1(6), thetaman, orbFin(6), thetaMan, thetaMan, orbTrasf(6), orbTrasf(6) orbFin2(6)],[deltaT1, deltaT, deltaT2, deltaT3, deltaT4],Title,Maneuv_name,'dyn',0,[0, deltaV1, deltaV, deltaV2, deltaV3, deltaV4])
 
 
 
 %% 
 
-orbFin2 = orbFin; 
+mu = 398600;                              
+
+ptoIniz = [-3441.6408 -7752.3491 -3456.8431 ...
+4.9270 -0.5369 -4.0350]; 
+rIniz = [ptoIniz(1) ptoIniz(2) ptoIniz(3)]';                % vettore posizione
+vIniz = [ptoIniz(4) ptoIniz(5) ptoIniz(6)]'; 
+[orbIniz] = GEtoPF(rIniz, vIniz, mu) % da GE coordinate PF
+
+                                % [x y z vx vy vz]
+orbFin2 = [12930.0, 0.2055, rad2deg(1.5510)...
+rad2deg(2.6830),  rad2deg(0.4098), rad2deg(1.6940)]';  % [a e i RAAN omega theta]                               %orbFin = [7400 0.1 20 90 0 250];  
+%orbFin(3) = 20;
 
 [orbTrasf, deltaV1, deltaV2, deltaT, eTGEvett, hT, nT, thetaT1, thetaT2] = trasfDir(orbIniz,orbFin2); 
 
@@ -83,8 +91,24 @@ plotOrbit(orbTrasf,[thetaT1, thetaT2],deltaT,'diretto',[{'inizio'}, {'fine'}],'d
 
 %% 
 
+clc; 
+close all;
+clear; 
+addpath('..\plot');
+addpath('..\orbitalMechanics');
+mu = 398600;                              
+
+ptoIniz = [-3441.6408 -7752.3491 -3456.8431 ...
+4.9270 -0.5369 -4.0350]; 
+rIniz = [ptoIniz(1) ptoIniz(2) ptoIniz(3)]';                % vettore posizione
+vIniz = [ptoIniz(4) ptoIniz(5) ptoIniz(6)]';
 dTtot=0;
 dVtot=0;
+orbIniz = GEtoPF(rIniz, vIniz, mu);
+
+orbFin = [12930.0, 0.2055, rad2deg(1.5510)...
+rad2deg(2.6830),  rad2deg(0.4098), rad2deg(1.6940)]';
+
 
 rPIniz=orbIniz(1)*(1-orbIniz(2)^2)/(1+orbIniz(2));
 rAFin=orbFin(1)*(1-orbFin(2)^2)/(1+orbFin(2));
@@ -130,35 +154,3 @@ Maneuv_name=[{'initial point'};{'1st change of P arg'};{'tangent burn'};...
     {'1st bitangent burn'};{'second bitangent burn'};{'final point'}];          %percorro orb2      %percorro orb3   %percorro orb4      %percorro orb5     %percorro orb6  %percorro orbFin
                                                         %percorro orbIniz
 plotOrbit([orbIniz, orb2,orb3', orb4',orb5', orb6, orbFin],[orbIniz(6), thetaman1, orb2(6), thetaMan2, 0, orb4(6),      orb4(6), thetaman4, orb5(6), thetaMan5, 180, 0,         180, orbFin(6) ],[deltaT1, deltaT2, deltaT3, deltaT4, deltaT5, deltaT6, deltaT7],Title,Maneuv_name,'dyn',0,[0, deltaV1, deltaV2, 0, deltaV4, deltaV5, deltaV6, deltaV7])
-
-
-%% STRATEGY 3
-%Allontanamento iniziale - apo
-
-
-
-
-
-%% Inizio plot
-earth3D(1);
-orbit3D(orbIniz, 1); 
-
-%%
-orbit3D(orbFin, 1); 
-
-
-%% STRATEGY 4
-
-[orbTrasf, dV1, dV2, dT] = trasfDir(orbIniz, orbFin); 
-
-earth3D(1)
-orbit3D(orbIniz, 1)
-orbit3D(orbTrasf,1)
-orbit3D(orbFin, 1)
-
-deltaV = dV1 + dV2
-
-
-
-
-
