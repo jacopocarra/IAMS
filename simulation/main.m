168ace6501b3f15f1011b269f24f70fea1048729
im-brooh <92543510+im-brooh@users.noreply.github.com>
2021-11-04 00:14:33 +0100

    Strat 2 pa aggiunta al main

--- simulation/main.m
+++ simulation/main.m
@@ -57,4 +57,76 @@ h = animatedline;
  end
 %}
 
+%% STRAT 2 PA - calcolo
 
+dTtot=0;
+dVtot=0;
+
+rPIniz=orbIniz(1)*(1-orbIniz(2)^2)/(1+orbIniz(2));
+rAFin=orbFin(1)*(1-orbFin(2)^2)/(1+orbFin(2));
+rAllontanamento=0.5e5;
+
+[orb1, deltaV1, deltaT1, thetaman1] = cambioAnomaliaPericentro(orbIniz, 290); % 290 scelta arbitraria, otiimo sembra tra tra 289 e 291
+dVtot=dVtot+deltaV1;
+dTtot=dTtot+deltaT1;
+
+
+[orb2, deltaV2, deltaT2, thetaman2] = manovraTangente(orb1, (rPIniz+rAllontanamento)/2, 'per');
+dVtot=dVtot+deltaV2;
+dTtot=dTtot+deltaT2;
+
+[orb3, deltaV3, deltaT3, thetaman3] = cambioInclinazione(orb2, orbFin(3), orbFin(4));
+dVtot=dVtot+deltaV3;
+dTtot=dTtot+deltaT3;
+
+orb5=orbFin;
+orb5(5)=wrapTo360(orb3(5)); %sfasare di 180 per aa e pa, lasciare così per ap e pa
+
+[deltaV, deltaV4, deltaV5, orb4, deltaT, deltaT4, deltaT5, thetaman4] = manovraBitangenteEllittica(orb3, orb5, 'pa');
+dVtot=dVtot+deltaV4+deltaV5;
+dTtot=dTtot+deltaT4+deltaT5;
+
+
+[orb6, deltaV6, deltaT6, thetaman5] = cambioAnomaliaPericentro(orb5, orbFin(5));
+dVtot=dVtot+deltaV6;
+dTtot=dTtot+deltaT6;
+
+% orb6 == orbFin a meno del tratto ancora da percorrere -->deltaT7
+
+deltaT7=tempoVolo(orb6, orb6(6), orbFin(6));
+%dTtot=dTtot +deltaT7;
+
+tStrat2PA=duration(0,0,dTtot); %trascuro tempo per raggiungere p.to finale esatto, fermo il conto all'inserzione nell'orbita finale
+%dV=dVtot
+
+%% -plot statico
+earth3D(1); 
+orbit3D(orbIniz, 1)
+
+%thetaman1
+orbit3D(orb1, 1)
+
+%thetaman2
+orbit3D(orb2, 1)
+
+%thetaman3
+orbit3D(orb3, 1)
+
+%thetaman4
+orbit3D(orb4, 1)
+
+%thetaman5
+orbit3D(orb5, 1)
+
+orbit3D(orb6,1 )
+orbit3D(orbFin, 1)
+
+%% -plot dinamico
+
+Title = 'STRATEGY 2 - PA';
+Maneuv_name=[{'initial point'};{'1st change of P arg'};{'tangent burn'};...
+    {'inclination change'};{'1st bitangent burn'};...
+    {'2nd bitangent burn'};{'2nd change of P arg'};{'final point'}];          
+                                                            % |percorro orbIniz    | percorro orb1     | percorro orb2     | percorro orb3     | %percorro orb4     |%percorro orb5  percorro orb6
+plotOrbit([orbIniz, orb1 , orb2 , orb3 ,orb4 , orb5, orbFin],[orbIniz(6), thetaman1, orb1(6), thetaman2, orb2(6), thetaman3, orb3(6), thetaman4,        0, 180 ,            180,thetaman5, orb6(6), orbFin(6) ],[deltaT1, deltaT2, deltaT3, deltaT4, deltaT5, deltaT6, deltaT7],Title,Maneuv_name,'dyn',0,[0, deltaV1, deltaV2, deltaV3, deltaV4, deltaV5, deltaV6])
+% CORRETTA PORCODDIO SE CE NE HO MESSO DI TEMPO
