341d4cb336a0eb52d7a9a621b5cfb5896da9cb29
jacopocarra <jacopo.carradori@skywarder.eu>
2021-11-04 18:43:03 +0100

    new strategy
    change of shape, change of plane, change of w

--- simulation/tester.m
+++ simulation/tester.m
@@ -371,3 +371,22 @@ dV
 deltaV1 + deltaV2
 
 
+
+%% SHAPE-PLANE-W
+clc
+orbFin(1)/orbIniz(1);   % se < 11.94 non conviene biellittica
+
+orbFin1  = [orbFin(1);orbFin(2);orbIniz(3);orbIniz(4);orbIniz(5);orbIniz(6)];
+ [deltaVtot1, deltaV1, deltaV2, orbTrasf, deltaTtot1, deltaT1, deltaT2, thetaMan2] = manovraBitangenteEllittica(orbIniz, orbFin1, 'pa');
+[orbFin2, deltaV3, deltaT3, thetaMan3] = cambioInclinazione(orbFin1, orbFin(3), orbFin(4));
+[orbFin3, deltaV4, deltaT4, thetaman4] = cambioAnomaliaPericentro(orbFin2, orbFin(5));
+
+deltaVtot = deltaVtot1+deltaV3+deltaV4
+
+Title = 'STRATEGY strana'
+Maneuv_name=[{'initial point'};...
+    {'first bitangent maneuver'};{'second bitangent maneuver'};...
+    {'change of plane'};{'change of P arg'};...
+    {'final point'}];
+thetaStory = [orbIniz(6), thetaMan2, thetaMan2, 180, 180, thetaMan3, orbFin2(6),thetaman4, orbFin3(6), orbFin(6) ];
+plotOrbit([orbIniz, orbTrasf,orbFin1, orbFin2,orbFin3],thetaStory,[deltaT1, deltaT, deltaT2, deltaT3, deltaT4],Title,Maneuv_name,'stat',0,[0, deltaV1, deltaV, deltaV2, deltaV3, deltaV4])
