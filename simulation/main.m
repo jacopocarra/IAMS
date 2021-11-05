ae1e293cdb7d075c42fc7846bfb8095e8dbae690
im-brooh <92543510+im-brooh@users.noreply.github.com>
2021-11-05 13:06:46 +0100

    corretto strat 1 PA

--- simulation/main.m
+++ simulation/main.m
@@ -95,10 +95,10 @@ orbFin2 = orbFin;
 [deltaV2, deltaV3, deltaV4, orbTrasf, deltaT2, deltaT3, deltaT4, thetaman3] = manovraBitangenteEllittica(orbFin3, orbFin2, 'pa');
 
 deltaVtot = deltaV1+deltaV+deltaV2
-deltaT = deltaT1+deltaT+deltaT2
+deltaTtot = deltaT1+deltaT+deltaT2+ tempoVolo(orbFin2, 180, orbFin(6))
 
 thetaStory = [orbIniz(6), thetaman1, orbFin1(6), thetaman2, orbFin3(6), thetaman3, thetaman3, orbTrasf(6), orbTrasf(6), orbFin2(6)];
-Title = 'STRATEGY 1  pa'
+Title = 'STRATEGY 1  PA'
 Maneuv_name=[{'initial point'};{'change of plane'};{'change of P arg'};...
     {'first bitangent maneuver'};{'second bitangent maneuver'};...
     {'final point'}];
@@ -110,7 +110,7 @@ orbit3D(orbIniz, 1);         % plot 3D orbita iniziale
 orbit3D(orbFin2, 1);                                      % plot 3D orbita finale
 %%
 close all
-plotOrbit([orbIniz, orbFin1,orbFin3, orbTrasf,orbFin2],thetaStory,[deltaT1, deltaT, deltaT2, deltaT3, deltaT4],Title,Maneuv_name,'stat',0,[0, deltaV1, deltaV, deltaV2, deltaV3, deltaV4])
+plotOrbit([orbIniz, orbFin1,orbFin3, orbTrasf,orbFin2],thetaStory,[deltaT1, deltaT, deltaT3, deltaT4, tempoVolo(orbFin2, 180, orbFin(6))],Title,Maneuv_name,'stat',0,[0, deltaV1, deltaV, deltaV3, deltaV4])
 
 
 
